00c76d0419e29920d84b1a0f2579ebfd4c66574f
ReEngine <amelin.it@gmail.com>
2020-09-22 00:30:09 +0300

    Did some basic screen and grid initialization, added some 2D graphics functions (draw a dot on a grid, draw a line on a grid)

---
+++ Grid.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace RayMarching
+{
+    class Grid
+    {
+        static int windowX = Console.LargestWindowWidth;
+        static int windowY = Console.LargestWindowHeight;
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+
+        static public int Width;
+        static public int Height;
+
+        static public void InitializeGrid()
+        {
+            Width = Console.WindowWidth / 2;
+            Height = Console.WindowHeight;
+        }
+        static public void InitializeScreen()
+        {
+
+            if (windowX % 2 != 0)
+            {
+                windowX--;
+            }
+            Console.CursorVisible = false;
+            Console.SetWindowSize(windowX, windowY);
+            ShowWindow(ThisConsole, 3);
+            Console.CursorVisible = false;
+
+        }
+
+        static bool Swap<T>(ref T x, ref T y)
+        {
+            try
+            {
+                T t = y;
+                y = x;
+                x = t;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static public void DrawPoint(int x, int y)
+        {
+            Console.SetCursorPosition(x * 2, y);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("██");
+        }
+        static public void DrawLine(int x0, int y0, int x1, int y1)
+        {
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                Swap<int>(ref x0, ref y0);
+                Swap<int>(ref x1, ref y1);
+            }
+
+            if (x0 > x1)
+            {
+                Swap<int>(ref x0, ref x1);
+                Swap<int>(ref y0, ref y1);
+            }
+
+            int dx, dy;
+            dx = x1 - x0;
+            dy = Math.Abs(y1 - y0);
+
+            int err = dx / 2;
+            int ystep;
+
+            if (y0 < y1)
+            {
+                ystep = 1;
+            }
+            else
+            {
+                ystep = -1;
+            }
+
+            for (; x0 <= x1; x0++)
+            {
+                if (steep)
+                {
+                    DrawPoint(y0, x0);
+                }
+                else
+                {
+                    DrawPoint(x0, y0);
+                }
+                err -= dy;
+                if (err < 0)
+                {
+                    y0 += ystep;
+                    err += dx;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+
 namespace RayMarching
 {
     class Program
     {
+        static int windowX = Console.LargestWindowWidth;
+        static int windowY = Console.LargestWindowHeight;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            if (args.Length != 0)
+            {
+                windowX = Convert.ToInt32(args[0]);
+                windowY = Convert.ToInt32(args[1]);
+            }
+
+
+
+            Grid.InitializeScreen();
+            Grid.InitializeGrid();
+            
+
+            //Grid.DrawPoint(4,4);
+            Console.ReadKey();
+
         }
+        
     }
 }
